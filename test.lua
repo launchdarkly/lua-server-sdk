5eb0fdca1aa29169f3a7f68222d4a176c265167d
hroederld <hroeder@launchdarkly.com>
2020-07-10 13:07:26 -0700

    [ch82565] collect between tests (#21)

--- test.lua
+++ test.lua
@@ -8,9 +8,12 @@ end
 
 l.registerLogger("TRACE", logger)
 
+TestAll = {}
+
 function makeTestClient()
     local c = l.clientInit({
-        key = "sdk-test"
+        key     = "sdk-test",
+        offline = true
     }, 0)
 
     return c
@@ -18,35 +21,40 @@ end
 
 local user = l.makeUser({ key = "alice" })
 
-function testBoolVariation()
+function TestAll:tearDown()
+    collectgarbage("collect")
+end
+
+function TestAll:testBoolVariation()
     local e = false
     u.assertEquals(e, makeTestClient().boolVariation(user, "test", e))
 end
 
-function testIntVariation()
+function TestAll:testIntVariation()
     local e = 3
     u.assertEquals(e, makeTestClient().intVariation(user, "test", e))
 end
 
-function testDoubleVariation()
+function TestAll:testDoubleVariation()
     local e = 5.3
     u.assertEquals(e, makeTestClient().doubleVariation(user, "test", e))
 end
 
-function testStringVariation()
+function TestAll:testStringVariation()
     local e = "a"
     u.assertEquals(e, makeTestClient().stringVariation(user, "test", e))
 end
 
-function testJSONVariation()
+function TestAll:testJSONVariation()
     local e = { ["a"] = "b" }
     u.assertEquals(e, makeTestClient().jsonVariation(user, "test", e))
 end
 
-function testRedisBasic()
+function TestAll:testRedisBasic()
     local c = l.clientInit({
         key                 = "sdk-test",
-        featureStoreBackend = r.makeStore({})
+        featureStoreBackend = r.makeStore({}),
+        offline             = true
     }, 0)
 
     local e = false
