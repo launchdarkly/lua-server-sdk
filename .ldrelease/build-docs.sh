34a9eef3191bedf97ad65663dea5fcb46bd36ebd
Casey Waldren <cwaldren@launchdarkly.com>
2022-02-08 11:21:11 -0800

    Fix Releaser v2 documentation generation & optimize build (#35)
    This commit fixes documentation generation by having ldoc directly write to the LD_RELEASE_DOCS_DIR path.
    It was previously broken because it was attempting to copy from PROJECT_DIR/docs to LD_RELEASE_DOCS_DIR, but ldoc actually creates a folder named "doc" by default (typo on my part.)
    
    Additionally, since there is no need to use CircleCI for Releaser builds, we can simply replace it with Releaser's docker functionality. This makes the build more stable since Releaser doesn't need to deal with CircleCI.

--- .ldrelease/build-docs.sh
+++ .ldrelease/build-docs.sh
@@ -4,12 +4,8 @@ set -e
 
 # This only runs in the Linux build, since the docs are the same for all platforms.
 
-PROJECT_DIR=$(pwd)
-
 mkdir tmp
 cp launchdarkly-server-sdk.c tmp/
 cp launchdarkly-server-sdk-redis.c tmp/
 
-ldoc tmp
-
-cp -r ${PROJECT_DIR}/docs/* ${LD_RELEASE_DOCS_DIR}
+ldoc -d ${LD_RELEASE_DOCS_DIR} tmp
