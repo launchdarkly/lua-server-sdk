05aeef5140885214f2fe4e2c114db5f0aaa43dc6
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2022-02-08 13:03:14 -0800

    prepare 1.2.1 release (#22)

--- .ldrelease/build-docs.sh
+++ .ldrelease/build-docs.sh
@@ -4,14 +4,8 @@ set -e
 
 # This only runs in the Linux build, since the docs are the same for all platforms.
 
-PROJECT_DIR=$(pwd)
-
 mkdir tmp
 cp launchdarkly-server-sdk.c tmp/
 cp launchdarkly-server-sdk-redis.c tmp/
 
-ldoc tmp
-
-mkdir -p $PROJECT_DIR/artifacts
-cd $PROJECT_DIR/doc
-zip -r $PROJECT_DIR/artifacts/docs.zip *
+ldoc -d ${LD_RELEASE_DOCS_DIR} tmp
