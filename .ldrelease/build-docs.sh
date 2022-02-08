46360da66c323053ae7ee72656553526bbe2cd65
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2022-02-08 13:44:05 -0800

    prepare 1.2.2 release (#24)

--- .ldrelease/build-docs.sh
+++ .ldrelease/build-docs.sh
@@ -4,8 +4,11 @@ set -e
 
 # This only runs in the Linux build, since the docs are the same for all platforms.
 
-mkdir tmp
-cp launchdarkly-server-sdk.c tmp/
-cp launchdarkly-server-sdk-redis.c tmp/
+# LD_RELEASE_TEMP_DIR is guaranteed to be empty, and will not be checked into version control.
+DOCS_BUILD_DIR="$LD_RELEASE_TEMP_DIR/docs"
+mkdir "$DOCS_BUILD_DIR"
 
-ldoc -d ${LD_RELEASE_DOCS_DIR} tmp
+cp launchdarkly-server-sdk.c "$DOCS_BUILD_DIR"
+cp launchdarkly-server-sdk-redis.c "$DOCS_BUILD_DIR"
+
+ldoc -d "$LD_RELEASE_DOCS_DIR" "$DOCS_BUILD_DIR"
