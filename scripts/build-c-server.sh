abc7f650ff44936323e9faa959879eca377c0670
Casey Waldren <cwaldren@launchdarkly.com>
2023-12-12 14:59:28 -0800

    fetch cpp server sdk v3

--- scripts/build-c-server.sh
+++ scripts/build-c-server.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Usage: ./scripts/build-c-server.sh <tag>
+
 set -e
 
-git clone https://github.com/launchdarkly/c-server-sdk.git
+branch="main"
+
+if [ -z "$1" ]
+then
+    branch="$1"
+fi
+
+
+git clone --depth 1 --branch "$branch" https://github.com/launchdarkly/cpp-sdks.git
 cd c-server-sdk
 mkdir build
 cd build
-cmake -D BUILD_SHARED_LIBS=ON -D BUILD_TESTING=OFF -D REDIS_STORE=ON ..
-make
-sudo make install
+cmake -D LD_BUILD_SHARED_LIBS=ON -D BUILD_TESTING=OFF -D LD_BUILD_EXAMPLES=OFF-D LD_BUILD_REDIS_SUPPORT=ON ..
+cmake --build . --target launchdarkly-cpp-server-redis-source
+cmake --install .
