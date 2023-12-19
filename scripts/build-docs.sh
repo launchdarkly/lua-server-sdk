0a112b9fbd6a05e45bfc483f7454ff6757efc04e
Casey Waldren <cwaldren@launchdarkly.com>
2023-12-18 16:20:53 -0800

    ci: fix docs publishing workflow (#30)
    Adds missing `luaVersion` workflow parameter to manual-build-docs. Also
    ensures `ldoc` is install on the CI runner.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -3,16 +3,19 @@
 set -e
 
 # The first argument is the directory where the docs should be built.
+# The second is where the docs should be copied to when built.
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <docs-build-dir>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <docs build dir> <docs release dir>"
     exit 1
 fi
 
 DOCS_BUILD_DIR=$1
+DOCS_RELEASE_DIR=$2
+
 mkdir "$DOCS_BUILD_DIR"
 
 cp launchdarkly-server-sdk.c "$DOCS_BUILD_DIR"
 cp launchdarkly-server-sdk-redis.c "$DOCS_BUILD_DIR"
 
-ldoc -d "$LD_RELEASE_DOCS_DIR" "$DOCS_BUILD_DIR"
+ldoc -d "$DOCS_RELEASE_DIR" "$DOCS_BUILD_DIR"
