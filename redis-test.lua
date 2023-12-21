56d8432690f6539ac7379d89b5b05cbcfcc8c6e7
Casey Waldren <cwaldren@launchdarkly.com>
2023-12-20 18:58:43 -0800

    update redis tests

--- redis-test.lua
+++ redis-test.lua
@@ -35,9 +35,21 @@ function TestAll:tearDown()
     collectgarbage("collect")
 end
 
-function TestAll:testRedisBasic()
+function TestAll:testVariationWithRedisSource()
     local e = false
     u.assertEquals(e, makeTestClient():boolVariation(user, "test", e))
+end
+
+function TestAll:testVariationDetailWithRedisSource()
+    local e = {
+        value  = true,
+        reason = {
+            kind      = "ERROR",
+            errorKind = "CLIENT_NOT_READY",
+            inExperiment = false
+        }
+    }
+    u.assertEquals(makeTestClient():boolVariationDetail(user, "test", true), e)
 end
 
 local runner = u.LuaUnit.new()
