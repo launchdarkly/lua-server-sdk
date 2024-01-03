88d8628a9aa3361b7f95476fea278fbe6cb7b4c0
Casey Waldren <cwaldren@launchdarkly.com>
2024-01-02 17:23:33 -0800

    add committing

--- scripts/update-rockspec-version.sh
+++ scripts/update-rockspec-version.sh
@@ -3,23 +3,35 @@
 set -e
 
 # This script has two responsibilities:
-# 1. Update the version in the rockspec file.
+# 1. Update the package version in the rockspec file.
 # 2. Update the actual filename of the rockspec file.
+# These two should match.
+# This script doesn't update the rockspec version - the part after the package version, but before the .rockspec
+# suffix. That should be done manually if the rockspec itself has changed but not the package contents.
 #
 # For example, if the current version is 1.0.0, then the rockspec filename is
 # launchdarkly-server-sdk-1.0.0-0.rockspec.
 #
 # If the new version is 1.2.0, then we want the new filename to be
 # launchdarkly-server-sdk-1.2.0-0.rockspec.
-#
-# The '-0' represents the rockspec revision itself (no source changes, just build changes.) This script doesn't
-# handle updating that - it should be done manually.
+
 
 input_rockspec=$1
 input_version=$2
+git_username=$3
+git_email=$4
 
+autocommit=''
+if [ -n "$git_username" ] || [ -n "$git_email" ]; then
+    autocommit=1
+fi
+
+# Ensure autocommit if provided is
 if [ -z "$input_rockspec" ] || [ -z "$input_version" ]; then
-    echo "Usage: $0 <rockspec package name without version> <target version>"
+    echo "Usage: $0 <rockspec package> <new version> [git username] [git email]"
+    echo "Example usage locally: $0 launchdarkly-server-sdk 1.2.0"
+    echo "Example usage in CI: $0 launchdarkly-server-sdk 1.2.0 LaunchDarklyReleaseBot LaunchDarklyReleaseBot@launchdarkly.com"
+    echo "Providing a git username and email will automatically commit any changes."
     exit 1
 fi
 
@@ -35,29 +47,41 @@ for file in "$input_rockspec"-*.rockspec; do
     # Strip off the prefix + '-' so only the semver, rockspec revision, and file extension remain.
     version_suffix="${file#$input_rockspec-}"
 
-
-
-
     IFS='-' read -ra parts <<< "$version_suffix"
     semver="${parts[0]}"
     rockspec="${parts[1]}"
 
-    # Split the suffix by '.' and take the first part, which is the rockspec revision.
+    # Split the suffix by '.' so we can get the rockspec version number.
     IFS='.' read -ra rockspec_parts <<< "$rockspec"
 
-    rockspec_version="${rockspec_parts[0]}"
+    rockspec_revision="${rockspec_parts[0]}"
     echo "Rockspec found: $file"
     echo "  package version: $semver"
-    echo "  rockspec version: $rockspec_version"
+    echo "  rockspec revision: $rockspec_revision"
 
 
-
-    new_file_name="$input_rockspec-$input_version-$rockspec_version.rockspec"
+    new_file_name="$input_rockspec-$input_version-$rockspec_revision.rockspec"
     git mv "$file" "$new_file_name"
     echo "Renamed $file to $new_file_name"
 
-    sed -i .bak "s/version = \".*\"/version = \"$input_version-$rockspec_version\"/" "$new_file_name"
+    sed -i .bak "s/version = \".*\"/version = \"$input_version-$rockspec_revision\"/" "$new_file_name"
     echo "Bumped version from $semver to $input_version"
 
-    rm "$new_file_name.bak"
+    rm -f "$new_file_name.bak"
+
+    if [ "$(git status --porcelain | wc -l)" -gt 0 ]; then
+      if [ -n "$git_username" ]; then
+        git config user.name "$git_username"
+      fi
+      if [ -n "$git_email" ]; then
+        git config user.email "$git_email"
+      fi
+      git add "$new_file_name"
+      if [ $autocommit ]; then
+        git commit -m "chore: bump package version from $semver to $input_version"
+      else
+        echo "Changes staged, but not committed. Please commit manually."
+      fi
+      exit 0
+    fi
 done
