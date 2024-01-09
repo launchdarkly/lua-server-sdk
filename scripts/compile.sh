2759ea4642a390e1a45e525839688e771ecea51d
Casey Waldren <cwaldren@launchdarkly.com>
2024-01-09 09:09:55 -0800

    chore: update compile.sh with release-please

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,5 +4,8 @@
 # the paths fetched by ./get-cpp-sdk-locally.sh.
 
 set -e
-luarocks make launchdarkly-server-sdk-2.0.1-0.rockspec LD_DIR=./cpp-sdks/build/INSTALL
-luarocks make launchdarkly-server-sdk-redis-2.0.1-0.rockspec LDREDIS_DIR=./cpp-sdks/build/INSTALL
+
+version="2.0.1" # {x-release-please-version }
+
+luarocks make launchdarkly-server-sdk-$version-0.rockspec LD_DIR=./cpp-sdks/build/INSTALL
+luarocks make launchdarkly-server-sdk-redis-$version-0.rockspec LDREDIS_DIR=./cpp-sdks/build/INSTALL
