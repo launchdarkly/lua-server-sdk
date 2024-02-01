c37eb736f1a8706e4b39a19aecfcc8f14c5cd73d
Casey Waldren <cwaldren@launchdarkly.com>
2024-01-31 16:25:41 -0800

    update Dockerfile

--- examples/hello-haproxy/service.lua
+++ examples/hello-haproxy/service.lua
@@ -14,18 +14,22 @@ local YOUR_FEATURE_KEY = "my-boolean-flag"
 -- Allows the LaunchDarkly SDK key to be specified as an environment variable (LD_SDK_KEY)
 -- or locally in this example code (YOUR_SDK_KEY).
 function get_key_from_env_or(name, existing_key)
-    if existing_key ~= "" then
+    if existing_key ~= nil and existing_key ~= "" then
+        core.Debug("Using LaunchDarkly SDK key from service.lua file")
         return existing_key
     end
 
     local env_key = os.getenv("LD_" .. name)
-    if env_key ~= "" then
+    if env_key ~= nil and env_key ~= "" then
+        core.Debug("Using LaunchDarkly SDK key from LD_" .. name .. " environment variable")
         return env_key
     end
 
-    error("No " .. name .. " specified (use LD_" .. name .. " environment variable or set local YOUR_" .. name .. ")")
+    core.log(core.crit, "LaunchDarkly SDK key not provided! SDK won't be initialized.")
+    return ""
 end
 
+local config = {}
 local client = ld.clientInit(get_key_from_env_or("SDK_KEY", YOUR_SDK_KEY), 1000, config)
 
 core.register_service("launchdarkly", "http", function(applet)
