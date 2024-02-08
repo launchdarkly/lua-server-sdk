9560053096b5ba8d23714aa0204df585991089b6
Casey Waldren <cwaldren@launchdarkly.com>
2024-02-08 11:50:49 -0800

    debugging hello haproxy

--- examples/env-helper/get_from_env_or_default.lua
+++ examples/env-helper/get_from_env_or_default.lua
@@ -2,14 +2,17 @@
 -- from an environment variable.
 function get_from_env_or_default(env_variable_name, local_variable)
     if local_variable ~= nil and local_variable ~= "" then
+        core.Debug("Using LaunchDarkly SDK key from service.lua file")
         return local_variable
     end
 
     local env_var = os.getenv(env_variable_name)
     if env_var ~= nil and env_var ~= "" then
+        core.Debug("Using LaunchDarkly SDK key from " .. env_variable_name .. " environment variable")
         return env_var
     end
 
+ core.log(core.crit, "LaunchDarkly SDK key not provided! SDK won't be initialized.")
     return ""
 end
 
